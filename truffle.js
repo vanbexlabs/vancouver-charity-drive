ae2b5a588d12cd6126a058f08f0a5ee48d19dc8e
Brian Onn <brian@vanbex.com>
2019-04-20 18:50:44 -0700

    updated

--- truffle.js
+++ truffle.js
@@ -1,4 +1,11 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "minty",
+      port: 7545,
+      network_id: "*" // Match any network id
+    }
+  }
 };
